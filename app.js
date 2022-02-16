35e612e0c6ad95587003e77f04c1c022fcc5cfba
Sakha <sakha60@gmail.com>
2022-02-16 12:26:33 +0300

    Connected from workPC over Remote SSH

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get("/mysql", function(request, response){
+     
+    // response.send("<h1>MySQL</h1>");
+    response.sendFile(__dirname + "/public/mysql.html");
+});
+
 app.listen(3050, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
