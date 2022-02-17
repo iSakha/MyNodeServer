013726f4d51c0e57cdccebd815b97547b4bc5444
Sakha <sakha60@gmail.com>
2022-02-17 14:35:10 +0300

    Create xhr post request

--- app.js
+++ app.js
@@ -3,14 +3,20 @@ const express = require("express");
 const PORT = 3050;
 const app = express();
 
+const jsonParser = express.json();
+
 app.use(express.static(__dirname + "/public"));
 
 app.get("/mysql", function(request, response){
-     
-    // response.send("<h1>MySQL</h1>");
     response.sendFile(__dirname + "/public/mysql.html");
 });
 
+app.post("/mysql", jsonParser, function (request, response) {
+  console.log(request.body);
+  if(!request.body) return response.sendStatus(400);
+   
+  response.json(request.body); // отправляем пришедший ответ обратно
+});
 
 const mysql = require("mysql2");
 
--- public/script_mysql.js
+++ public/script_mysql.js
@@ -1,3 +1,5 @@
+
+
 document.getElementById('test').addEventListener('click', showTest);
 document.getElementById('work').addEventListener('click', showWork);
 
@@ -21,3 +23,25 @@ function hideDivs() {
     divTest.classList.add('d-none');
 }
 
+document.getElementById('btn-connect').addEventListener('click', function (e) {
+    e.preventDefault();
+    connectServer();
+});
+// document.getElementById('btn-connect').addEventListener('click', disconnectServer);
+
+function connectServer() {
+
+    let user = {};
+    user.name = "Johnson";
+    user.pass = "1234";
+    let user_str = JSON.stringify(user);
+    let request = new XMLHttpRequest();
+    request.open("POST", "/mysql", true);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.addEventListener("load", function () {
+        // получаем и парсим ответ сервера
+         let receivedUser = JSON.parse(request.response);
+         console.log(receivedUser);   // смотрим ответ сервера
+     });
+     request.send(user_str);
+}
