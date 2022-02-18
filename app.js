82c84816a1a3fdb83c1bb746742bbd864cb258ec
Sakha <sakha60@gmail.com>
2022-02-18 15:29:10 +0300

    Issue with return connection status

--- app.js
+++ app.js
@@ -2,50 +2,115 @@ const express = require("express");
 
 const PORT = 3050;
 const app = express();
+let userName;
+let userPass;
+let connection;
+let result = "";
 
 const jsonParser = express.json();
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/mysql", function(request, response){
-    response.sendFile(__dirname + "/public/mysql.html");
+app.get("/mysql", function (request, response) {
+  response.sendFile(__dirname + "/public/mysql.html");
 });
 
-app.post("/mysql", jsonParser, function (request, response) {
+app.post("/mysql/connect", jsonParser, function (request, response) {
   console.log(request.body);
-  if(!request.body) return response.sendStatus(400);
-   
-  response.json(request.body); // отправляем пришедший ответ обратно
+  getCreds(request.body);
+  let sqlResponse = connectToMysqlServer();
+  console.log(request.body);
+  console.log("res: " + sqlResponse);
+  if (!request.body) return response.sendStatus(400);
+
+  // response.json(request.body); // отправляем пришедший ответ обратно
+  response.json({ "result": "ok" });
+});
+
+app.post("/mysql/disconnect", jsonParser, function (request, response) {
+  console.log(request.body);
+  // getCreds(request.body);
+  closeConnection();
+  // closeConnection();
+  if (!request.body) return response.sendStatus(400);
+
+  // response.json(request.body); // отправляем пришедший ответ обратно
+  response.json({ "result": "disconnected" });
 });
 
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 23308,
+//   user: userName,
+//   // database: "usersdb",
+//   password: userPass
+// });
+
+
+function getCreds(user) {
+  console.log(user);
+  userName = user.name;
+  userPass = user.pass;
+}
+// ==========================================================================
+function openConnection() {
+
+  connection = mysql.createConnection({
     host: "localhost",
     port: 23308,
-    user: "root",
+    user: userName,
     // database: "usersdb",
-    password: "Sakha2836"
+    password: userPass
+    
+  });
+  
+return connection;
+  // return connectToMysqlServer();
+} 
+
+  function connectToMysqlServer() {
+    connection  = openConnection();
+
+    //    Test connection
+    connection.connect(function (err) {
+      if (err) {
+        console.error("Error: " + err.message);
+        result = "error"
+        console.log("result: " + result);
+        return result;
+      }
+      else {
+        console.log("Successfully connected to MysqlServer");
+        result = "success";
+        console.log("result: " + result);
+        return result;
+      }
+    });
+  }
+
+// ==========================================================================
+function closeConnection() {
+
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 23308,
+    user: userName,
+    // database: "usersdb",
+    password: userPass
   });
 
-  //    Test connection
-  connection.connect(function(err){
-    if (err) {
-      return console.error("Error: " + err.message);
-    }
-    else{
-      console.log("Successfully connected to MysqlServer");
-    }
- });
   //    Close connection
-  connection.end(function(err) {
+  connection.end(function (err) {
     if (err) {
       return console.log("Error: " + err.message);
     }
     console.log("Connection closed");
   });
+}
 
 app.listen(3050, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
 
--- public/script_mysql.js
+++ public/script_mysql.js
@@ -27,16 +27,19 @@ document.getElementById('btn-connect').addEventListener('click', function (e) {
     e.preventDefault();
     connectServer();
 });
-// document.getElementById('btn-connect').addEventListener('click', disconnectServer);
+document.getElementById('btn-disconnect').addEventListener('click', function (e) {
+    e.preventDefault();
+    disconnectServer();
+});
 
 function connectServer() {
 
     let user = {};
-    user.name = "Johnson";
-    user.pass = "1234";
+    user.name = document.getElementsByClassName('test-connect')[0].value;
+    user.pass = document.getElementsByClassName('test-connect')[1].value;
     let user_str = JSON.stringify(user);
     let request = new XMLHttpRequest();
-    request.open("POST", "/mysql", true);
+    request.open("POST", "/mysql/connect", true);
     request.setRequestHeader("Content-Type", "application/json");
     request.addEventListener("load", function () {
         // получаем и парсим ответ сервера
@@ -44,4 +47,20 @@ function connectServer() {
          console.log(receivedUser);   // смотрим ответ сервера
      });
      request.send(user_str);
+     document.getElementsByClassName('test-connect')[0].value = "";
+     document.getElementsByClassName('test-connect')[1].value = "";
+}
+
+function disconnectServer() {
+    let sendMessageObj = {"message": "disconnect"};
+    let sendMessageStr = JSON.stringify(sendMessageObj);
+    let request = new XMLHttpRequest();
+    request.open("POST", "/mysql/disconnect", true);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.addEventListener("load", function () {
+        // получаем и парсим ответ сервера
+         let receivedUser = JSON.parse(request.response);
+         console.log(receivedUser);   // смотрим ответ сервера
+     });
+     request.send(sendMessageStr);
 }
