252992b00660d2aaa766def5d8504b474df38503
Sakha <sakha60@gmail.com>
2022-03-22 17:24:35 +0300

    Get calendars and get events

--- public/js/script_testpage.js
+++ public/js/script_testpage.js
@@ -1,6 +1,6 @@
 
-
-
+//          Ping host
+// ==============================================================================
 document.getElementById("btn-ping").addEventListener("click", function (e) {
     let ipAddress = document.getElementById("ip-address").value;
     document.getElementById("ping-responce").innerHTML = "";
@@ -28,9 +28,83 @@ function displayResponce(data) {
     document.getElementById("ping-responce").innerHTML = data.msg;
 }
 
-
-document.getElementById("btn-open-page").addEventListener("click", openMidServerPage)
+//          Check is mid_server running
+// ==============================================================================
+document.getElementById("btn-open-page").addEventListener("click", openMidServerPage);
 
 function openMidServerPage() {
     window.open("http://82.209.203.205:3055", "_blank");
+}
+
+//          Get list of calendars from MySql server 
+//          using mid_server
+// ==============================================================================
+document.getElementById("btn-get-calendars").addEventListener("click", getCalendarsList);
+
+function getCalendarsList() {
+
+    // using fetch
+
+    fetch('http://82.209.203.205:3055/calendars', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: ""
+    })
+        .then(res => res.json())
+        .then(data => {
+            // enter you logic when the fetch is successful
+            console.log(data);
+            createHtmlCalendarList(data);
+        })
+        .catch(error => {
+            // enter your logic for when there is an error (ex. error toast)
+            console.log(error)
+        })
+
+}
+
+function createHtmlCalendarList(data) {
+    let calendarDiv = document.getElementById("calendar-list");
+    let listElement = document.createElement('ul');
+
+    calendarDiv.appendChild(listElement);
+
+    for (let i = 0; i < data.length; i++) {
+        let listItem = document.createElement('li');
+        listItem.innerHTML = data[i].cal_name;
+        listElement.appendChild(listItem);
+    }
+
+}
+
+//          Get list of events from MySql server 
+//          using mid_server
+// ==============================================================================
+
+document.getElementById("btn-get-events").addEventListener("click", getEventsList);
+
+function getEventsList() {
+
+    // using fetch
+
+    fetch('http://82.209.203.205:3055/events', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: ""
+    })
+        .then(res => res.json())
+        .then(data => {
+            // enter you logic when the fetch is successful
+            console.log(data);
+            // createHtmlCalendarList(data);
+        })
+        .catch(error => {
+            // enter your logic for when there is an error (ex. error toast)
+            console.log(error)
+        })
+
 }
